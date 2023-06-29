d79f9cd46d8449e0a2ea9c5ef05d7b5d03f9cae8
MstyslavDmytryk <mstyslavdmytryk@gmail.com>
2023-06-29 11:02:12 +0300

    1929 solution added

---
+++ solutions/1929. Concatenation of Array.js
@@ -0,0 +1,12 @@
+// 29.06.2023
+// Mstyslav Dmytryk
+// https://leetcode.com/problems/concatenation-of-array/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    return [...nums, ...nums]
+    
+};
