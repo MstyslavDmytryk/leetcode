8386d4e370315f7c3dfda126b1f299fd74c24273
MstyslavDmytryk <mstyslavdmytryk@gmail.com>
2023-06-29 10:55:48 +0300

    solution added

---
+++ solutions/2703. Return Length of Arguments Passed.js
@@ -0,0 +1,11 @@
+// 29.06.2023
+// Mstyslav Dmytryk
+// https://leetcode.com/problems/return-length-of-arguments-passed/
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return arguments.length
+};
+
